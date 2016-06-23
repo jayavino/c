9ec8a0313e87f8cc32257498a18eaec5673c85bd
jayavino <vinonw13@gmail.com>
2016-06-23 10:22:24 +0530

    Create g.c

---
+++ g.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main()
+{
+    int range,i=0,sum=0;
+    scanf("%d",&range);
+    for(i=0;i<=range;i++)
+    {
+    sum=sum+i;
+    }
+    printf("\nThe sum of natural number is %d:",sum);
+    return 0;
+}
+
+    Status API Training Shop Blog About 
+
+    © 2016 GitHub, Inc. Terms
