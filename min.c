bef83b907cad8a31128b5dcf436b0556d58c9a0d
jayavino <vinonw13@gmail.com>
2016-06-23 14:39:51 +0530

    Create min.c

---
+++ min.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+int main()
+{
+    int a, b, i, flag;
+   // printf("Enter two numbers(intevals): ");
+    scanf("%d %d", &a, &b);
+   if(a<b)
+   {
+    printf("%d is min",a);
+   }
+    else
+    {
+    printf("%d is min",b);
+}
+    return 0;
+}
