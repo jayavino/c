f08d84521f2f8ed5b502a81647081deb38d036c9
jayavino <vinonw13@gmail.com>
2016-06-23 09:42:54 +0530

    Create h.c

---
+++ h.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main()
+{
+    int number;
+    scanf("%d",&number);
+    if(number>=0)
+    {
+        printf("\nEntered number is positive ");
+    }
+    else
+    {
+        printf("\nEntered number is negative");
+    }
+    return 0;
+}
